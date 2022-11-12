69cc777eab14b175142a3d295ee36f08e7a2bc1a
Shahnazul <mdshahnazulislam@gmail.com>
2022-11-13 00:35:06 +0600

    modify

--- php_file_othentication/fetch_function.php
+++ php_file_othentication/fetch_function.php
@@ -4,18 +4,21 @@ $conn = mysqli_connect("localhost", "root", "", "test") or die("Connection Faild
 $sql = "SELECT * FROM student";
 $result = mysqli_query($conn, $sql) or die("Query Faild");
 
-// $row = mysqli_fetch_assoc($result);
+$row = mysqli_fetch_field($result);
 
-/*echo "<pre>";
+echo "<pre>";
 print_r($row);
 echo "</pre>";
-*/
-/*echo $row['firstname']. " " .$row['lastname'];*/
 
-while($row = mysqli_fetch_assoc($result)){
+/*foreach ($row as $data) {
+  echo $data['id']. " ". $data['firstname']. " ". $data['lastname']. " ". $data['email']. "<br>";
+}*/
+
+//echo $row[1]. " " .$row[2];
+
+/*while($row = mysqli_fetch_assoc($result)){
   echo $row['firstname']. " " .$row['lastname'] . "<br>";
-}
+}*/
 
-echo "shakil khan";
 
 ?>
