a33b672e39e3cdfe1a6b3acba1f2423aab8a2ad6
shahnazulislam <mdshahnazulislam@gmail.com>
2022-11-12 17:08:39 +0600

    add some code here

--- php_file_othentication/form.php
+++ php_file_othentication/form.php
@@ -1,4 +1,21 @@
 <?php
-echo "hellow world";
+   $var = 38;
+
+   var_dump(filter_var($var,FILTER_VALIDATE_FLOAT));
+   $options = array(
+    "option" => array(
+        "main_range"=>20,
+        "max_range"=>30
+    )
+    );
+
+    if(filter_var($var, FILTER_VALIDATE_FLOAT, $iptions)){
+        echo "<br>$var is float";
+    }else{
+        echo "<br>$var is not float";
+    }
+
+    
+    
 
 ?>
